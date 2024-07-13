89da5e8eaf04613a7b0a0905bf75a984b5860dc3
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-07-13 14:09:57 -0500

    Fetching address from the comment section too

--- Create Address Change Ticket/createAdressUpdateTicket.js
+++ Create Address Change Ticket/createAdressUpdateTicket.js
@@ -5,7 +5,13 @@ function fetchTicketDetails(){
     var policyNo = splitTicketName[0].trim();
     var ownerName = splitTicketName[1].trim();
 
-    return [policyNo, ownerName]
+    const commentSection = document.getElementsByClassName("ak-renderer-document");
+    const addressLine = commentSection[0].children[8].innerHTML;
+    
+    var address = addressLine.split("-")[1].trim();
+    
+
+    return [policyNo, ownerName,address]
 }
 
 console.log(fetchTicketDetails());
