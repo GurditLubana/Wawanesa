2c459e2d5d7c2bd78cadcb0322d9ff11dad2e51e
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-07-13 22:13:38 -0500

    env variables added

--- Email-JIRA-Creator/executeCode.py
+++ Email-JIRA-Creator/executeCode.py
@@ -3,36 +3,25 @@ import os
 
 outlook = win32.Dispatch('outlook.application')
 namespace = outlook.GetNamespace('MAPI')
-directory = "C:\\Users\\gurditsingh\\OneDrive - Wawanesa\\Desktop\\Incoming Mails\\July\\July 09\\Life services"
-
 
 def incomingMails():
     accounts = namespace.Accounts
-
     account_index = 4 # for incoming mails 
-
     print("Sending email from: " + accounts.Item(account_index).DisplayName)
-
+    directory = "C:\\Users\\gurditsingh\\OneDrive - Wawanesa\\Desktop\\Incoming Mails\\July\\July 09\\Life services"
     for fileName in os.listdir(directory):
         if fileName.lower().endswith('.pdf'):
             mail = outlook.CreateItem(0)
             mail.Body = "Good afternoon,\n\nPlease find the new mail we received today.\n\nRegards"
             mail.Subject = fileName
             mail.Attachments.Add(directory + "\\" + fileName)
-            mail.To = 'lifeservices@wawanesa.com'
-
+            mail.To = "LIFE SERVICE EMAIL"
             mail._oleobj_.Invoke(*(64209, 0, 8, 0, accounts.Item(account_index)))
-
-
             try:
                 # mail.Send()
                 print('Email sent successfully for ' + fileName)
             except Exception as e:
                 print(f'Error: {e}')
-
-
-
-
 
 
 
@@ -43,7 +32,6 @@ def returnMails():
     directory = mainDirectory + currentDirectory
 
     accounts = namespace.Accounts
-
     account_index = 3
 
     print("Sending email from: " + accounts.Item(account_index).DisplayName)
@@ -54,7 +42,7 @@ def returnMails():
             # mail.Body = "Good afternoon,\nPlease find the mail we received today.\nRegards"
             mail.Subject = fileName
             mail.Attachments.Add(directory + "\\" + fileName)
-            mail.To = 'JSD-Life-Annuity@jira.wawanesa.com'
+            mail.To = 'JSD LIFE ANNUTIES EMAIL'
 
             mail._oleobj_.Invoke(*(64209, 0, 8, 0, accounts.Item(account_index)))
 
