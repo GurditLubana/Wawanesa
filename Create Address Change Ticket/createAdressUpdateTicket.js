b714f6a77d5eb7c62feae98de535b63279cc81ca
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-07-13 21:43:39 -0500

    Added final submitCreate function

--- Create Address Change Ticket/createAdressUpdateTicket.js
+++ Create Address Change Ticket/createAdressUpdateTicket.js
@@ -1,6 +1,4 @@
-
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-
 function fetchTicketDetails() {
     const summaryBtn = document.getElementsByClassName("css-t0icuu");
     const ticketName = summaryBtn[0].getAttribute('aria-label');
@@ -12,20 +10,17 @@ function fetchTicketDetails() {
     var address = addressLine.split("-")[1].trim();
     return [policyNo, ownerName, address]
 }
-
 function setInput(inputElement, inputValue) {
     const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
     nativeInputValueSetter.call(inputElement, inputValue);
     inputElement.dispatchEvent(new Event('input', { bubbles: true }));
     inputElement.dispatchEvent(new Event('change', { bubbles: true }));
 }
-
 function setSummary(summaryString) {
     const inputSummary = document.getElementById("summary-field");
     setInput(inputSummary, summaryString);
     inputSummary.click();
 }
-
 function setChngTeam() {
     var chngLabel = document.getElementById("customfield_17511-field-label");
     var parentDiv = (chngLabel.closest('div')).lastChild.firstChild;
@@ -33,34 +28,30 @@ function setChngTeam() {
     setInput(inputChangeTeam, "Reception");
     document.getElementsByClassName("select-customfield_17511__menu-list")[0].firstChild.click();
 }
-
 function setCategory() {
-
     var categoryInput = document.getElementsByClassName("select-customfield_12810__input-container")[0];
     setInput(categoryInput.firstChild, "Address/Phone number");
     document.getElementsByClassName("select-customfield_12810__menu-list")[0].firstChild.click();
 }
-
 function setSource() {
-
     var categoryInput = document.getElementsByClassName("select-customfield_11118__input-container")[0];
     setInput(categoryInput.firstChild, "Phone");
     document.getElementsByClassName("select-customfield_11118__menu-list")[0].firstChild.click();
 }
-
 function setPolicyNumber(policyNumber) {
     var policyInput = document.getElementById("customfield_15113-field");
     setInput(policyInput, policyNumber);
 }
-
 function assignToMe() {
     var assignTomeBtn = document.getElementsByClassName("css-nsst23")[1];
     assignTomeBtn.click();
-
 }
-
+function clickCreateBtn(){
+    var submitCreateBtn = document.getElementsByClassName("css-1jk3zmn")[0];
+    console.log(submitCreateBtn);
+    // submitCreateBtn.click();
+}
 async function setDescription(newAddress) {
-
     var date = new Date();
     var options = { year: 'numeric', month: 'long', day: 'numeric' };
     var todaysDate = date.toLocaleDateString('en-US', options);    
@@ -85,14 +76,12 @@ async function setDescription(newAddress) {
     await delay(500); 
 
 }
-
 async function createAddressChangeTicket() {
-
     const summaryInfo = fetchTicketDetails();
     const policyNumber = summaryInfo[0];
     const policyOwnerName = summaryInfo[1];
     const newAddress = summaryInfo[2];
-    const summaryString = policyNumber + " - " + policyOwnerName;
+    const summaryString = "Address Update Request - Policy # " + policyNumber + " - " + policyOwnerName;
     const createBtn = document.getElementById("createGlobalItem");
     createBtn.click();
     await delay(1500);
@@ -104,8 +93,6 @@ async function createAddressChangeTicket() {
     setPolicyNumber(policyNumber);
     assignToMe();
     await setDescription(newAddress);
-
+    clickCreateBtn();
 }
-
-
 createAddressChangeTicket();
