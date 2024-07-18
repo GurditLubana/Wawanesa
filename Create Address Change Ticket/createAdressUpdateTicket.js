f0cbe12d34e2ec6a6cb2ec02ac79cafa381fd921
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-07-17 20:52:12 -0500

    Fixed the split address from every -  issue

--- Create Address Change Ticket/createAdressUpdateTicket.js
+++ Create Address Change Ticket/createAdressUpdateTicket.js
@@ -1,7 +1,7 @@
 javascript: (
     function () {
         const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-        function fetchTicketDetails() {
+         function fetchTicketDetails() {
             const summaryBtn = document.getElementsByClassName("css-t0icuu");
             const ticketName = summaryBtn[0].getAttribute('aria-label');
             const splitTicketName = ticketName.split("-");
@@ -9,7 +9,8 @@ javascript: (
             var ownerName = splitTicketName[1].trim();
             const commentSection = document.getElementsByClassName("ak-renderer-document");
             const addressLine = commentSection[0].children[8].innerHTML;
-            var address = addressLine.split("-")[1].trim();
+            var index = addressLine.indexOf('-');
+            var address = addressLine.substring(index + 1).trim();
             return [policyNo, ownerName, address]
         }
         function setInput(inputElement, inputValue) {
