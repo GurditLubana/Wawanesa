5e2f2f1b6c2783a6887d4582f1b14a89d0a30af2
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-09-07 20:56:14 -0500

    Fixed the delete policy whose address we received issue

--- Return-mail-report/generateReport.js
+++ Return-mail-report/generateReport.js
@@ -1,5 +1,5 @@
-javascript:(async function(){
-async function simulateScroll(element) {
+javascript: (async function () {
+    async function simulateScroll(element) {
     const distance = 2;
     const boundingRect = element.getBoundingClientRect();
     const startX = boundingRect.left + (boundingRect.width / 2);
@@ -28,24 +28,42 @@ var navbar = (document.getElementsByClassName("css-3b7q7p")[0]).children[1];
 var navLength = navbar.children.length;
 const adrsChngNav = navbar.children[navLength - 3];
 const rtrnMailNav = navbar.children[navLength - 4];
-console.log(adrsChngNav);
-console.log(rtrnMailNav);
-let rtrnMailPolicyList = new Set();
-let currentDate = new Date();
+const currentDate = new Date();
 console.log("Please wait, while report is being generated...");
 let monthsArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 let targetMonth = monthsArray[currentDate.getMonth() - 6];
-function addPolicy(policyNumber) {
-    if (!rtrnMailPolicyList.has(policyNumber)) {
-        rtrnMailPolicyList.add(policyNumber);
+console.log("Target month is", targetMonth);
+let rtrnMailPolicyList = {};
+let rcvdAdrsList = {};
+for (let i = 0; i < 6; i++) {
+    rtrnMailPolicyList[monthsArray[currentDate.getMonth() - i]] = new Set();
+    rcvdAdrsList[monthsArray[currentDate.getMonth() - i]] = new Set();
+}
+function addPolicy(policyNumber, month,list) {
+    let currMonthList = list[month];
+    if (!currMonthList.has(policyNumber)) {
+        currMonthList.add(policyNumber);
     }
 }
 function deletePolicy(policyNumber) {
-    if (rtrnMailPolicyList.has(policyNumber)) {
-        rtrnMailPolicyList.delete(policyNumber);
-        return true;
+    let result = false;
+    for (let i = 0; i < 6; i++) {
+        let currMonthList = rtrnMailPolicyList[monthsArray[currentDate.getMonth() - i]];
+        if (currMonthList.has(policyNumber)) {
+            (rtrnMailPolicyList[monthsArray[currentDate.getMonth() - i]]).delete(policyNumber);
+            addPolicy(policyNumber, monthsArray[currentDate.getMonth() - i],rcvdAdrsList);
+            result = true;
+        }
     }
-    return false;
+    return result;
+}
+function countTotalMails(rtrnMailPolicyList){
+    let finalCount = 0;
+    for(let x = 0; x < 6; x++){
+        let currMonthList = rtrnMailPolicyList[monthsArray[currentDate.getMonth() - x]];
+        finalCount += currMonthList.size;
+    }
+    return finalCount;
 }
 function extractPolicyNum(policyNumText, isRMreport) {
     if (isRMreport) {
@@ -64,7 +82,7 @@ function extractPolicyNum(policyNumText, isRMreport) {
     }
     else {
 
-        let regex = /Policy #\s*:?\s*(\S+)/;
+        let regex = /#\s*:?\s*(\S+)/;
         let match = policyNumText.match(regex);
         let policyNumber = match ? match[1] : null;
         return policyNumber;
@@ -85,26 +103,26 @@ async function generateReport(isRMreport) {
             else {
                 var eachCol = eachTicket.querySelectorAll("._1e0c1txw._4cvr1h6o._1tke1ylp._vchhusvi._1bsb1osq");
                 if (eachCol[8] && eachCol[8].firstChild && eachCol[8].firstChild.firstChild && eachCol[8].firstChild.firstChild.firstChild) {
-                    var resolutionDate = eachCol[8].firstChild.firstChild.firstChild.innerText;
+                    var dateCreated = eachCol[4].firstChild.firstChild.innerText;
                     var policyNumText = (eachCol[1].firstChild.firstChild.firstChild.innerText);
                     var policyNum = extractPolicyNum(policyNumText, isRMreport);
-                    if (isRMreport) { addPolicy(policyNum); } else {
-                        countAdrsChanges += deletePolicy(policyNum) ? 1 : 0;
-                    }
-                    let month = resolutionDate.substring(0, 3);
+                    let month = dateCreated.substring(3, 6);
                     if (month === targetMonth) {
                         break;
+                    }
+                    if (isRMreport) { addPolicy(policyNum, month,rtrnMailPolicyList); } else {
+                        countAdrsChanges += deletePolicy(policyNum) ? 1 : 0;
                     }
                 }
             }
         }
         catch (e) {
-            console.error("here is the error", eachTicket, i, e);
-            break;
+            console.error("here is the error", eachTicket, e);
+
         }
     }
-    var listName = isRMreport ? "Return Mails" : "Address Changes";
-    var count = isRMreport ? rtrnMailPolicyList.size : countAdrsChanges;
+    var listName = isRMreport ? "Return Mails" : "New Addresses";
+    var count = isRMreport ? countTotalMails(rtrnMailPolicyList) : countAdrsChanges;
     console.log("There were total", count, listName, "received from the month of", monthsArray[currentDate.getMonth() - 5], "to", monthsArray[currentDate.getMonth()]);
 }
 rtrnMailNav.firstChild.click();
@@ -117,7 +135,9 @@ let finalTime = new Date();
 let diff = finalTime - currentDate;
 let minutes = Math.floor(diff / 60000);
 let seconds = ((diff % 60000) / 1000).toFixed(0);
+console.log("List of Policies whose address we never received: ",rtrnMailPolicyList);
+console.log("List of Policies whose address we received: ",rcvdAdrsList);
 let executionTime = "Program took " + minutes + " minutes and " + seconds + " seconds to execute.";
-console.log("Total number of policies with DONOT mail: ", rtrnMailPolicyList.size, "(Meaning we didn't receive new address for these policies)");
+console.log("Total number of policies with DONOT mail: ", countTotalMails(rtrnMailPolicyList), "(Meaning we didn't receive new address for these policies)");
 console.log(executionTime);
-console.log(rtrnMailPolicyList);})();
+})();
