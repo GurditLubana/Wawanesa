e126d56b39a9402bf9f572a59642ae28ecc4f4fc
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-09-14 23:53:35 -0500

    Updated rtmLists, sorted list first, more detailed output results

--- Return-mail-report/generateReport.js
+++ Return-mail-report/generateReport.js
@@ -1,29 +1,4 @@
 javascript: (async function () {
-    async function simulateScroll(element) {
-    const distance = 2;
-    const boundingRect = element.getBoundingClientRect();
-    const startX = boundingRect.left + (boundingRect.width / 2);
-    const startY = boundingRect.top + (boundingRect.height / 2);
-    element.dispatchEvent(new MouseEvent('mousedown', {
-        clientX: startX,
-        clientY: startY,
-        bubbles: true
-    }));
-    for (let step = 0; step < distance; step += 1) {
-        element.dispatchEvent(new MouseEvent('mousemove', {
-            clientX: startX,
-            clientY: startY + step,
-            bubbles: true
-        }));
-        await new Promise(resolve => setTimeout(resolve, 100));
-    }
-    element.dispatchEvent(new MouseEvent('mouseup', {
-        clientX: startX,
-        clientY: startY + distance,
-        bubbles: true
-    }));
-    await new Promise(resolve => setTimeout(resolve, 1000));
-}
 var navbar = (document.getElementsByClassName("css-3b7q7p")[0]).children[1];
 var navLength = navbar.children.length;
 const adrsChngNav = navbar.children[navLength - 3];
@@ -38,6 +13,31 @@ let rcvdAdrsList = {};
 for (let i = 0; i < 6; i++) {
     rtrnMailPolicyList[monthsArray[currentDate.getMonth() - i]] = new Set();
     rcvdAdrsList[monthsArray[currentDate.getMonth() - i]] = new Set();
+}
+async function simulateScroll(element) {
+        const distance = 2;
+        const boundingRect = element.getBoundingClientRect();
+        const startX = boundingRect.left + (boundingRect.width / 2);
+        const startY = boundingRect.top + (boundingRect.height / 2);
+        element.dispatchEvent(new MouseEvent('mousedown', {
+            clientX: startX,
+            clientY: startY,
+            bubbles: true
+        }));
+        for (let step = 0; step < distance; step += 1) {
+            element.dispatchEvent(new MouseEvent('mousemove', {
+                clientX: startX,
+                clientY: startY + step,
+                bubbles: true
+            }));
+            await new Promise(resolve => setTimeout(resolve, 100));
+        }
+        element.dispatchEvent(new MouseEvent('mouseup', {
+            clientX: startX,
+            clientY: startY + distance,
+            bubbles: true
+        }));
+        await new Promise(resolve => setTimeout(resolve, 2500));
 }
 function addPolicy(policyNumber, month,list) {
     let currMonthList = list[month];
@@ -88,7 +88,17 @@ function extractPolicyNum(policyNumText, isRMreport) {
         return policyNumber;
     }
 }
+async function sortCreatedList(){
+    var createdBtn = document.querySelector(".css-1078jy");
+    createdBtn.click();
+    await new Promise(resolve => setTimeout(resolve, 4000));
+    createdBtn = document.querySelector(".css-1078jy");
+    createdBtn.click();
+    await new Promise(resolve => setTimeout(resolve, 4000));
+}
+
 async function generateReport(isRMreport) {
+    await sortCreatedList();
     const rtnMailsList = document.querySelectorAll("._ca0q1b66._n3tdhkvd")[0];
     const scrollElement = document.querySelector('.track-vertical').firstChild;
     var allTickets = rtnMailsList.children;
@@ -102,8 +112,8 @@ async function generateReport(isRMreport) {
             }
             else {
                 var eachCol = eachTicket.querySelectorAll("._1e0c1txw._4cvr1h6o._1tke1ylp._vchhusvi._1bsb1osq");
-                if (eachCol[8] && eachCol[8].firstChild && eachCol[8].firstChild.firstChild && eachCol[8].firstChild.firstChild.firstChild) {
-                    var dateCreated = eachCol[4].firstChild.firstChild.innerText;
+                if (eachCol[2]) {
+                    var dateCreated = eachCol[2].firstChild.firstChild.innerText;
                     var policyNumText = (eachCol[1].firstChild.firstChild.firstChild.innerText);
                     var policyNum = extractPolicyNum(policyNumText, isRMreport);
                     let month = dateCreated.substring(3, 6);
@@ -135,9 +145,16 @@ let finalTime = new Date();
 let diff = finalTime - currentDate;
 let minutes = Math.floor(diff / 60000);
 let seconds = ((diff % 60000) / 1000).toFixed(0);
-console.log("List of Policies whose address we never received: ",rtrnMailPolicyList);
-console.log("List of Policies whose address we received: ",rcvdAdrsList);
+console.log("\n");
+for(var i = 0; i<6 ; i++ ){
+    var rtnMailTotalCount = rtrnMailPolicyList[monthsArray[currentDate.getMonth() - i]].size;
+    var adrsRcvdTotalCount = rcvdAdrsList[monthsArray[currentDate.getMonth() - i]].size;
+    var total = rtnMailTotalCount + adrsRcvdTotalCount;
+    console.log("In ", monthsArray[currentDate.getMonth() - i], " we received total ", adrsRcvdTotalCount, " new addresses out of ", rtnMailTotalCount, " return mails.")
+}
+console.log("\nList of Policies whose address we never received: ",rtrnMailPolicyList);
+console.log("List of Policies whose address we received: ",rcvdAdrsList,"\n");
 let executionTime = "Program took " + minutes + " minutes and " + seconds + " seconds to execute.";
-console.log("Total number of policies with DONOT mail: ", countTotalMails(rtrnMailPolicyList), "(Meaning we didn't receive new address for these policies)");
+console.log("\nTotal number of policies with DONOT mail: ", countTotalMails(rtrnMailPolicyList), "(Meaning we didn't receive new address for these policies)");
 console.log(executionTime);
 })();
