68ff769904572b9cda0c6b44c3c777034e9269dc
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-10-20 18:30:18 -0500

    Fixed select Document Type error
    The source code of the website was changed, and hence program wasn't able to select the document type automatically. Fixed it by changing the className in selectDocType

--- Auto-Upload-Return-Mails/executeAutomation.js
+++ Auto-Upload-Return-Mails/executeAutomation.js
@@ -1,92 +1,90 @@
 javascript: (
-    function () {
-        function extractPolicyNumber(fileName) {
-            var splitFileName = (fileName.split(' '));
-            var result = [splitFileName[0].trim()];
-            var a = 1;
-            var breakLoop = false;
-            while(a < splitFileName.length && breakLoop === false){
-                if(splitFileName[a] === "&"){
-                    result.push(splitFileName[a+1].trim());
-                    a = a + 2;
-                    }
-                else if(splitFileName[a] === "-"){breakLoop = true;}
-                else{ 
-                    a++;}
-            }
-            
-            return result;
+    function () {function extractPolicyNumber(fileName) {
+        var splitFileName = (fileName.split(' '));
+        var result = [splitFileName[0].trim()];
+        var a = 1;
+        var breakLoop = false;
+        while(a < splitFileName.length && breakLoop === false){
+            if(splitFileName[a] === "&"){
+                result.push(splitFileName[a+1].trim());
+                a = a + 2;
+                }
+            else if(splitFileName[a] === "-"){breakLoop = true;}
+            else{ 
+                a++;}
         }
-        async function selectDocType(checkbox, delay,rows) {
-            if (checkbox && !checkbox.checked) {
-                checkbox.click();
-            }
-            await delay(5);
-            var dropdown = rows[0].children[4];
-            dropdown.click();
-            await delay(5);
-            var dropdownMenu = document.getElementsByClassName("k-animation-container");
-            var ulContext = dropdownMenu[0].firstChild.lastChild.children[1].firstChild;
-            ulContext.children[7].click();
-            document.getElementById("_cascadeValuesButton_en").click();
-            await delay(5);
+        
+        return result;
+    }
+    async function selectDocType(checkbox, delay,rows) {
+        if (checkbox && !checkbox.checked) {
+            checkbox.click();
         }
-        async function processRows() {
-            const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-            var tableBody = document.querySelector('.k-grid-content .k-selectable tbody');
-            if (tableBody) {
-                var rows = tableBody.children;
-                var checkbox = rows[0].querySelector('input[type="checkbox"]');
+        await delay(15);
+        var dropdown = rows[0].children[4];
+        dropdown.click();
+        await delay(10);
+        var dropdownMenu = document.getElementsByClassName("k-child-animation-container");
+        var ulContext = dropdownMenu[0].firstChild.lastChild.children[1].firstChild.children[7];
+        ulContext.click();
+        document.getElementById("_cascadeValuesButton_en").click();
+        await delay(5);
+    }
+    async function processRows() {
+        const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+        var tableBody = document.querySelector('.k-grid-content .k-selectable tbody');
+        if (tableBody) {
+            var rows = tableBody.children;
+            var checkbox = rows[0].querySelector('input[type="checkbox"]');
+            document.querySelectorAll('.k-grid-content .k-selectable tbody tr input[type="checkbox"]').forEach(cb => {
+                if (cb.checked) {
+                    cb.click();
+                }
+            });
+            await selectDocType(checkbox, delay, rows);
+            for (var i = 0; i < rows.length; i++) {
+                var checkbox = rows[i].querySelector('input[type="checkbox"]');
                 document.querySelectorAll('.k-grid-content .k-selectable tbody tr input[type="checkbox"]').forEach(cb => {
                     if (cb.checked) {
                         cb.click();
                     }
                 });
-                await selectDocType(checkbox, delay, rows);
-                for (var i = 0; i < rows.length; i++) {
-                    var checkbox = rows[i].querySelector('input[type="checkbox"]');
-                    document.querySelectorAll('.k-grid-content .k-selectable tbody tr input[type="checkbox"]').forEach(cb => {
-                        if (cb.checked) {
-                            cb.click();
-                        }
-                    });
 
-                    if (checkbox && !checkbox.checked) {
-                        checkbox.click();
-                    }
+                if (checkbox && !checkbox.checked) {
+                    checkbox.click();
+                }
 
+                await delay(5);
+
+                var docName = (rows[i].children[2]).innerText;
+                var policyNumArray = extractPolicyNumber(docName);
+                for(var x in policyNumArray){
+                    var policyNum = policyNumArray[x];
+                    var numberInput = document.getElementById("_number_text");
+                    numberInput.value = policyNum;
+
+                    var addButton = document.getElementById("_AddButton");
+                    addButton.click();
                     await delay(5);
 
-                    var docName = (rows[i].children[2]).innerText;
-                    var policyNumArray = extractPolicyNumber(docName);
-                    for(var x in policyNumArray){
-                        var policyNum = policyNumArray[x];
-                        var numberInput = document.getElementById("_number_text");
-                        numberInput.value = policyNum;
-    
-                        var addButton = document.getElementById("_AddButton");
-                        addButton.click();
-                        await delay(5);
-    
-                        var updateButton = document.getElementById("_update_selected");
-                        updateButton.click();
-                        await delay(5);
-                    }
-                    var clearButton = document.getElementById("_ClearButton");
-                    clearButton.click();
-                    await delay(5);
-                    rows[i].children[6].click();
-                    await delay(5);
-                    var docNameInput = document.getElementById("docName");
-                    docNameInput.value = '';
-                    docNameInput.value = 'Return mail';
-                    docNameInput.dispatchEvent(new Event('input', { bubbles: true }));
-                    docNameInput.dispatchEvent(new Event('change', { bubbles: true }));
+                    var updateButton = document.getElementById("_update_selected");
+                    updateButton.click();
                     await delay(5);
                 }
-            } else {
-                console.error('Table body not found.');
+                var clearButton = document.getElementById("_ClearButton");
+                clearButton.click();
+                await delay(5);
+                rows[i].children[6].click();
+                await delay(5);
+                var docNameInput = document.getElementById("docName");
+                docNameInput.value = '';
+                docNameInput.value = 'Return mail';
+                docNameInput.dispatchEvent(new Event('input', { bubbles: true }));
+                docNameInput.dispatchEvent(new Event('change', { bubbles: true }));
+                await delay(5);
             }
+        } else {
+            console.error('Table body not found.');
         }
-        processRows();
-    })();
+    }
+    processRows();})();
